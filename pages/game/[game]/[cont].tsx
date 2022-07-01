8dde4a526b13e542a7bc80c07544001ff31b7ad9
Panayiotis Lipiridis <lipiridis@gmail.com>
2022-07-01 20:23:45 +0300

    yes

--- pages/game/[game]/[cont].tsx
+++ pages/game/[game]/[cont].tsx
@@ -21,7 +21,7 @@ const CONTINENTS = [
   ["oceania", "Oceania"],
 ];
 
-const shuffleArray = (array: T[]) => {
+const shuffleArray = (array: any) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
